4b6dc59fea81d4512deb36952f1ea0c830806f5f
wuxiao <wuxiao@uwsclient-165-183.uws.ualberta.ca>
2013-04-26 14:13:30 -0600

    gridview

--- gg/gen/com/example/gg/R.java
+++ gg/gen/com/example/gg/R.java
@@ -27,13 +27,15 @@ public final class R {
         public static final int youtube=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080004;
+        public static final int gridView1=0x7f080003;
         public static final int imageButton1=0x7f080001;
         public static final int imageButton2=0x7f080002;
         public static final int mediaController1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int youku=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
