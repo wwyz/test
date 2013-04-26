c7cc64e442969b3476d6eb7b80c4fa7e64a58b7f
wuxiao <wuxiao@uwsclient-165-183.uws.ualberta.ca>
2013-04-26 13:28:49 -0600

    youtube icon

--- gg/src/com/example/gg/MainActivity.java
+++ gg/src/com/example/gg/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.gg;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends Activity {
-
-    @Override
+    
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Intent playIntent = new Intent(Intent.ACTION_VIEW);
+        playIntent.setData(Uri.parse("http://www.youtube.com/watch?v=sMM0R19IisI"));
+        playIntent.putExtra("force_fullscreen", true);
+        
+        
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=sMM0R19IisI"));
+        intent.putExtra("force_fullscreen",true); 
+        startActivity(intent);
+       // Intent intent = new Intent(this, MainActivity.class);
+
+
+        //MainActivity.this.startActivity(playIntent);
     }
 
 
