77d90e870061206e29dd15caf56fd913289685fc
wuxiao <wuxiao@uwsclient-165-183.uws.ualberta.ca>
2013-04-26 14:18:30 -0600

    lack of grid

--- gg/src/com/example/gg/Youku.java
+++ gg/src/com/example/gg/Youku.java
@@ -19,7 +19,7 @@ public class Youku extends Activity{
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.youku);
 
         
 //        Intent playIntent = new Intent(Intent.ACTION_VIEW);
