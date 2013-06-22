64bc015ae3d7955b63de47b244c2e73ac4ada97d
shuyi3 <bugdx123@gmail.com>
2013-06-22 13:11:33 -0600

    data

---
+++ gg/src/com/examples/gg/Data.java
@@ -0,0 +1,7 @@
+package com.examples.gg;
+
+public class Data {
+	
+	public static final UploaderData dotacinema = new UploaderData("thumbnaiapil", "playlistsapi");
+
+}
---
+++ gg/src/com/examples/gg/UploaderData.java
@@ -0,0 +1,13 @@
+package com.examples.gg;
+
+public class UploaderData
+{
+    private String name;
+    private String avatar;
+
+    public UploaderData(String name, String avatar){
+    	this.name = name;
+    	this.avatar = avatar;
+    	
+    }
+}
